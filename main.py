c66a9263ba5b749efb1e88b170976bf92c36eca5
mohammadanarul <mdanarul7075@gmail.com>
2022-09-10 22:48:46 +0600

    fastapi hello world.

--- main.py
+++ main.py
@@ -6,3 +6,43 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'hello': 'hello world'}
+
+
+@app.get('/products/')
+def product_list():
+    products = [
+        {
+            'id': 1,
+            'title': 'product-1',
+            'price': 1500,
+            'brand': 'asus',
+            'category': 'Laptop'
+        },
+        {
+            'id': 2,
+            'title': 'product-2',
+            'price': 2000,
+            'brand': 'apple',
+            'category': 'Laptop'
+        },
+        {
+            'id': 3,
+            'title': 'product-1',
+            'price': 2555,
+            'brand': 'Dell',
+            'category': 'Laptop'
+        },
+        {
+            'id': 4,
+            'title': 'product-1',
+            'price': 3000,
+            'brand': 'hp',
+            'category': 'Laptop'
+        }
+    ]
+    return {'products': products}
+
+@app.get('/products/{id}')
+def product_detail(id:  int):
+    print('product',id)
+    return {'product': {'id': id, 'title': f'product-{id}', 'price': 152+id}}
